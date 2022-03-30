5a61faff4d9c3f569195114586e8fbed5e311c3d
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-03-30 12:21:58 -0400

    Update movie-view.jsx
    Added back button method

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export class MovieView extends React.Component {
 
     render() {
-        const { movie } = this.props;
+        const { movie, onBackClick } = this.props;
 
         return (
             <div className='movie-view'>
                 <div className='movie-poster'>
-                    <img src={movieData.ImagePath} />
+                    <img src={movie.ImagePath} />
                 </div>
                 <div className='movie-title'>
                     <span className='label'>Title: </span>
@@ -18,6 +18,7 @@ export class MovieView extends React.Component {
                     <span className='label'>Description: </span>
                     <span className='value'>{movie.Description}</span>
                 </div>
+                <button onClick={() => { onBackClick(null); }}> Back</button>
 
             </div>
         );
