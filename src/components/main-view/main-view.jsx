49b8ef6362c6d8c6de114f3069e057646ffb49c5
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-03-31 12:45:11 -0400

    Update main-view.jsx
    Attempting absolute path for image

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -9,22 +9,28 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [],
+            movies: [
+                {
+                    _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: <img src="https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/"></img>,
+                },
+                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
+                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
+            ],
             selectedMovie: null
         }
     }
 
-    componentDidMount() {
-        axios.get('https://my-flix-2022.herokuapp.com/movies')
-            .then(response => {
-                this.setState({
-                    movies: response.data
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+    //componentDidMount() {
+    //    axios.get('https://my-flix-2022.herokuapp.com/movies')
+    //        .then(response => {
+    //            this.setState({
+    //                movies: response.data
+    //            });
+    //        })
+    //        .catch(error => {
+    //            console.log(error);
+    //        });
+    // }
 
     setSelectedMovie(newSelectedMovie) {
         this.setState({
