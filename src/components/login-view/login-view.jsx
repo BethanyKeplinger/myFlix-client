6d5051b21749600c3a41dddf7f0d82b0cb263d30
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-01 14:02:19 -0400

    Create login-view.jsx

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        /*Send a request to the server for aunthentication */
+        /*the call props.onLoggedIn(username) */
+        props.onLoggedIn(username);
+    };
+
+    return (
+        <form>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <label>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
+            </label>
+        </form>
+    );
+}
