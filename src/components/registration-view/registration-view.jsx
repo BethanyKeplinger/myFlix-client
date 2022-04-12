091355c00f66ee0719cb6ad5a70079b7f0526026
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-11 21:56:49 -0400

    Update registration-view.jsx
    Used bootstrap to create new registration form

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Navbar, Nav, Form, Button, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
+
+import './registration-view.scss';
 
 export function RegistrationView(props) {
     const [username, setUsername] = useState('');
@@ -14,24 +18,78 @@ export function RegistrationView(props) {
 
     return (
 
-        <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <label>
-                Email:
-                <input type="text" value={email} onChange={e => setEmail(e, target.value)} />
-            </label>
-            <label>
-                Birthday:
-                <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-            </label>
-            <button type="submit" onClick={handleSubmit}>Register</button>
-        </form>
-    )
+        <Container fluid className="registerContainer">
+
+            <Navbar bg="navColor" variant="dark" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#home">MyFlix</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="logout">Register</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card className="registerCard">
+                            <Card.Body>
+                                <Card.Title className="text-center"> Welcome to MyFlix</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted text-center">Please Register</Card.Subtitle>
+
+                                <Form>
+                                    <Form.Group>
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            value={username}
+                                            onChange={e => setUsername(e.target.value)}
+                                            placeholder="Enter a username"
+                                        />
+
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            value={password}
+                                            onChange={e => setPassword(e.target.value)}
+                                            placeholder="Enter a password with 6 or more characters"
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control
+                                            type="email"
+                                            value={email}
+                                            onChange={e => setEmail(e.target.value)}
+                                            placeholder="Enter a valid email address"
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Birthday</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            value={birthday}
+                                            onChange={e => setBirthday(e.target.value)}
+                                        />
+                                    </Form.Group>
+
+                                    <Button className="registerButton" variant="secondary" type="submit" onClick={handleSubmit}>Register</Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
+
+    );
 }
+
+RegistrationView.propTypes = {
+    onRegistration: PropTypes.func.isRequired,
+};
