b12b2c5ef53b1a8548edcdc5007f433f7d32c2b1
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-12 14:31:26 -0400

    Update login-view.jsx
    Imported more react bootstrap items. Created new form.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Navbar, Nav, Form, Button, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
@@ -16,19 +15,46 @@ export function LoginView(props) {
     };
 
     return (
-        <Form>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
-                <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-            </Form.Group>
+        <Container fluid className="loginContainer">
 
-            <Form.Group controlId="formUsername">
-                <Form.label>Password:</Form.label>
-                <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit
-            </Button>
-        </Form>
+            <Navbar bg="navColor" variant="dark" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#home">MyFlix</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="login">Login</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card className="loginCard">
+                            <Card.Body>
+                                <Card.Title className="text-center"> Welcome to MyFlix</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted text-center">Please Login</Card.Subtitle>
+
+                                <Form>
+                                    <Form.Group controlId="formUsername">
+                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                                    </Form.Group>
+
+                                    <Form.Group controlId="formUsername">
+                                        <Form.label>Password:</Form.label>
+                                        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                                    </Form.Group>
+
+                                    <Button variant="primary" type="submit" onClick={handleSubmit}>Login
+                                    </Button>
+
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
