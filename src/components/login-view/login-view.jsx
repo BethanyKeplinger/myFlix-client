3255cda51491682eb2a85c9d2bb4d9fc92cdf4b8
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-15 12:49:43 -0400

    Update login-view.jsx
    Added user authorization to login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,12 +9,38 @@ import "./login-view.scss";
 import axios from 'axios';
 >>>>>>> Stashed changes
 
+import axios from 'axios';
+
 export function LoginView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    //Declare hook for each input
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+
+    //validate user inputs
+    const validate = () => {
+        let isReq = true;
+        if (!username) {
+            setUsernameErr('Username Required');
+            isReq = false;
+        } else if (username.length < 4) {
+            setUsernameErr('Username must be at least 4 characters long');
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr('Password Required');
+            isReq = false;
+        } else if (password.length < 6) {
+            setPasswordErr('Password must be at least 6 characters long');
+            isReq = false;
+        }
+        return isReq;
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+<<<<<<< Updated upstream
         axios.post('https://my-flix-2022.herokuapp.com/login', {
             Username: username,
             Password: password
@@ -29,6 +55,23 @@ export function LoginView(props) {
             .catch(e => {
                 console.log('no such user')
             });
+=======
+        const isReq = validate();
+        if (isReq) {
+            /*Send a request to the server for authentication*/
+            axios.post('https://my-flix-2022.herokuapp.com/login', {
+                Username: username,
+                Password: password
+            })
+                .then(response => {
+                    const data = response.data;
+                    props.onLoggedIn(data);
+                })
+                .catch(e => {
+                    console.log('no such user')
+                });
+        }
+>>>>>>> Stashed changes
     };
 
     return (
@@ -55,11 +98,21 @@ export function LoginView(props) {
                                     <Form.Group controlId="formUsername">
                                         <Form.Label>Username:</Form.Label>
                                         <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                                        {/*code added here to display validation error*/}
+                                        {usernameErr && <p> {usernameErr}</p>}
                                     </Form.Group>
 
+<<<<<<< Updated upstream
                                     <Form.Group controlId="formUsername">
                                         <Form.label>Password:</Form.label>
                                         <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+=======
+                                    <Form.Group controlId="formPassword">
+                                        <Form.Label className="form-element">Password:</Form.Label>
+                                        <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
+                                        {/*code added here to display validation error*/}
+                                        {passwordErr && <p>{passwordErr}</p>}
+>>>>>>> Stashed changes
                                     </Form.Group>
 
                                     <Button variant="primary" type="submit" onClick={handleSubmit}>Login
@@ -76,5 +129,9 @@ export function LoginView(props) {
 }
 
 LoginView.propTypes = {
+    user: PropTypes.shape({
+        Username: PropTypes.string.isRequired,
+        Password: PropTypes.string.isRequired,
+    }),
     onLoggedIn: PropTypes.func.isRequired
 };
