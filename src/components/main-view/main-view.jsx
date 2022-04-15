60ce5eb45b234f836fce9c9c22dbd99a4d7d5182
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-15 09:20:52 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -22,29 +22,8 @@ export class MainView extends React.Component {
         };
     }
 
-    getMovies(token) {
-        axios.get('https://my-flix-2022.herokuapp.com/movies', {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-            .then(response => {
-                //Assign the result to the state
-                this.setState({
-                    movies: response.data
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
-
-    componentDidMount() {
-
-        const token = localStorage.getItem("token")
-
-        axios.get('https://my-flix-2022.herokuapp.com/movies', {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-
+   componentDidMount() {
+        axios.get('https://my-flix-2022.herokuapp.com/movies')
             .then(response => {
                 this.setState({
                     movies: response.data
@@ -54,7 +33,6 @@ export class MainView extends React.Component {
                 console.log(error);
             });
     }
-
     /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
     setSelectedMovie(newSelectedMovie) {
         this.setState({
@@ -62,15 +40,10 @@ export class MainView extends React.Component {
         });
     }
     /* when a user successfully logs in, this function updates the `user` property in state to that particular user */
-    onLoggedIn(authData) {
-        console.log(authData);
+    onLoggedIn(user) {
         this.setState({
-            user: authData.user.Username
+            user
         });
-
-        localStorage.setItem('token', authData.token);
-        localStorage.setItem('user', authData.user.Username);
-        this.getMovies(authData.token);
     }
 
     onRegistration(register) {
@@ -82,10 +55,10 @@ export class MainView extends React.Component {
     render() {
         const { movies, selectedMovie, user, register } = this.state;
 
-        //if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)} />);
+        if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)} />);
 
         /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-        //if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
         //Before the movies have been loaded
         if (movies.length === 0) return <div className='main-view' />;
