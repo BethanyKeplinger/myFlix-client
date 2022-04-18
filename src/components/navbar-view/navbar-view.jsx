dd8cd43c7f1882eb55541f7c42f42df560eef13c
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-18 17:25:25 -0400

    Update navbar-view.jsx

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './navbar-view.scss';
 
-import { Navbar, Container, Nav, Button, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
 export function NavbarView({ user }) {
 
@@ -10,16 +10,39 @@ export function NavbarView({ user }) {
         window.open('/', '_self');
     };
 
+    const isAuth = () => {
+        if (typeof window == "undefined") {
+            return false;
+        }
+        if (localStorage.getItem("token")) {
+            return localStorage.getItem("token");
+        } else {
+            return false;
+        }
+    }
+
     return (
-        <Navbar id="navbar" fixed="top">
+        <Navbar id="navbar" fixed="top" expand="lg">
             <Container id="navbar-container">
-                <Navbar.Brand id="navbar-brand" href="#">MyFlix</Navbar.Brand>
-                <Nav id="id" className="me-auto">
-                    <Nav.Link id="nav-link" href="#home">Account</Nav.Link>
-                    <Nav.Link id="nav-link" href="#features">Watchlist</Nav.Link>
-                    <Nav.Link id="nav-link" href="#pricing">Register</Nav.Link>
-                </Nav>
+                <Navbar.Brand id="navbar-brand" href="/">MyFlix</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav id="id" className="ml-auto">
+                        {isAuth() && (
+                            <Nav.Link id="nav-link" href={`/users/${user}`}>{user}</Nav.Link>
+                        )}
+                        {isAuth() && (
+                            <Button vaariant="link" onClick={() => { this.onLoggedOut() }}>Logout</Button>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link id="nav-link" href="/">Sign-in</Nav.Link>
+                        )}
+                        {isAuth() && (
+                            <Nav.Link id="nav-link" href="/register">Sign-up</Nav.Link>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
+    );
 }
