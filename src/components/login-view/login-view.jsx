e7469e13ffd7f564eb7da9851482c46a1354f121
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-18 13:39:29 -0400

    Update login-view.jsx
    Added authorization

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -49,59 +49,60 @@ export function LoginView(props) {
                     console.log('no such user')
                 });
         }
-
-        return (
-            <Container fluid className="loginContainer">
-
-                <Navbar bg="navColor" variant="dark" expand="lg">
-                    <Container fluid>
-                        <Navbar.Brand href="#home">MyFlix</Navbar.Brand>
-                        <Nav className="me-auto">
-                            <Nav.Link href="login">Login</Nav.Link>
-                        </Nav>
-                    </Container>
-                </Navbar>
-
-                <Row>
-                    <Col>
-                        <CardGroup>
-                            <Card className="loginCard">
-                                <Card.Body>
-                                    <Card.Title className="text-center"> Welcome to MyFlix</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted text-center">Please Login</Card.Subtitle>
-
-                                    <Form>
-                                        <Form.Group controlId="formUsername">
-                                            <Form.Label>Username:</Form.Label>
-                                            <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-                                            {/*code added here to display validation error*/}
-                                            {usernameErr && <p>{usernameErr}</p>}
-                                        </Form.Group>
-
-                                        <Form.Group controlId="formPassword">
-                                            <Form.Label className="form-element">Password:</Form.Label>
-                                            <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
-                                            {/*code added here to display validation error*/}
-                                            {passwordErr && <p>{passwordErr}</p>}
-                                        </Form.Group>
-
-                                        <Button id="form-button" variant="primary" type="submit" onClick={handleSubmit}>Login
-                                        </Button>
-
-                                    </Form>
-                                </Card.Body>
-                            </Card>
-                        </CardGroup>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
-
-    LoginView.propTypes = {
-        register: PropTypes.shape({
-            Username: PropTypes.string.isRequired,
-            Password: PropTypes.string.isRequired,
-        }),
-        onLoggedIn: PropTypes.func.isRequired
     };
+
+    return (
+        <Container fluid className="loginContainer">
+
+            <Navbar bg="navColor" variant="dark" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#home">MyFlix</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="login">Login</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card className="loginCard">
+                            <Card.Body>
+                                <Card.Title className="text-center"> Welcome to MyFlix</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted text-center">Please Login</Card.Subtitle>
+
+                                <Form>
+                                    <Form.Group controlId="formUsername">
+                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                                        {/*code added here to display validation error*/}
+                                        {usernameErr && <p>{usernameErr}</p>}
+                                    </Form.Group>
+
+                                    <Form.Group controlId="formPassword">
+                                        <Form.Label className="form-element">Password:</Form.Label>
+                                        <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
+                                        {/*code added here to display validation error*/}
+                                        {passwordErr && <p>{passwordErr}</p>}
+                                    </Form.Group>
+
+                                    <Button id="form-button" variant="primary" type="submit" onClick={handleSubmit}>Login
+                                    </Button>
+
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+LoginView.propTypes = {
+    register: PropTypes.shape({
+        Username: PropTypes.string.isRequired,
+        Password: PropTypes.string.isRequired,
+    }),
+    onLoggedIn: PropTypes.func.isRequired
+};
