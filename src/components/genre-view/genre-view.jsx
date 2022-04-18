ae1bafc0b03bdea1fe9f390b6794bd78c86ce531
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-18 17:24:05 -0400

    Create genre-view.jsx

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './genre-view.scss';
+
+import { Container, Col, Row, Card, Button, Cardgroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export class GenreView extends React.Component {
+
+    render() {
+        const { genre, onBackClick } = this.props;
+
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <Card id="director-view">
+                            <Card.Body>
+                                <Card.Title>{genre.Name}</Card.Title>
+                                <Card.Text>
+                                    Description: {genre.Description}
+                                </Card.Text>
+                                <Button id="genre-back-button" onClick={() => { onBackClick(); }}>Back</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
