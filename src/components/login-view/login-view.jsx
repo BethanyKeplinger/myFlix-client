483bb64c89a4300ed52e46910e6f5c57f231674a
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-18 12:43:07 -0400

    login validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -8,6 +8,30 @@ import axios from 'axios';
 export function LoginView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    //Declare hook for each input
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, sertPasswordErr] = useState('');
+
+    //validate user inputs
+    const validate = () => {
+        let isReq = true;
+        if (!username) {
+            setUsernameErr('Username Required');
+            isReq = false;
+        } else if (username.length < 4) {
+            setUsernameErr('Username must be at least 4 characters long');
+            isReq = false
+        }
+        if (!password) {
+            sertPasswordErr('Password Required');
+            isReq = false;
+        } else if (password.length < 6) {
+            sertPasswordErr('Password must be at least 6 characters long');
+            isReq = false;
+        }
+
+        return isReq;
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -50,11 +74,15 @@ export function LoginView(props) {
                                         <Form.Group controlId="formUsername">
                                             <Form.Label>Username:</Form.Label>
                                             <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                                            {/*code added here to display validation error*/}
+                                            {usernameErr && <p>{usernameErr}</p>}
                                         </Form.Group>
 
                                         <Form.Group controlId="formPassword">
                                             <Form.Label className="form-element">Password:</Form.Label>
                                             <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
+                                            {/*code added here to display validation error*/}
+                                            {passwordErr && <p>{passwordErr}</p>}
                                         </Form.Group>
 
                                         <Button id="form-button" variant="primary" type="submit" onClick={handleSubmit}>Login
@@ -71,5 +99,9 @@ export function LoginView(props) {
     }
 
     LoginView.propTypes = {
+        register: PropTypes.shape({
+            Username: PropTypes.string.isRequired,
+            Password: PropTypes.string.isRequired,
+        }),
         onLoggedIn: PropTypes.func.isRequired
     };
