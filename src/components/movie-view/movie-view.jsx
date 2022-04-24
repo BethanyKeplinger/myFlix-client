37a185f0dc0e0369eee16756074e34659816f8c9
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-24 19:49:02 -0400

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
@@ -12,11 +13,20 @@ export class MovieView extends React.Component {
                     <Col>
                         <Card id="movie-view">
                             <Card.Body>
-                                <Card.Img id="movie-view-image" variant="top" src={movie.ImagePath} />
+                                <Card.Img crossOrigin="anonymous" id="movie-view-image" variant="top" src={movie.ImagePath} />
                                 <Card.Title id="movie-title" className="movie-title">{movie.Title}</Card.Title>
                                 <Card.Text id="movie-description" className="movie-description">{movie.Description}</Card.Text>
                                 <Card.Text id="movie-director" className="movie-director">Director: {movie.Director.Name}</Card.Text>
                                 <Card.Text id="movie-genre" className="movie.genre">Genre: {movie.Genre.Name}</Card.Text>
+
+                                <Link to={`/director/${movie.Director.Name}`}>
+                                    <Button variant="link" id="movie-director" className="movie-director">
+                                        Director: {movie.Director.Name}</Button>
+                                </Link>
+                                <Link to={`/genre/${movie.Genre.Name}`}>
+                                    <Button variant="link" id="movie-genre" className="movie-gerne">
+                                        Genre: {movie.Genre.Name}</Button>
+                                </Link>
                             </Card.Body>
                         </Card>
 
@@ -29,3 +39,20 @@ export class MovieView extends React.Component {
         );
     }
 }
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        // Description: PropTypes.string.isRequired,
+        // Genre: PropTypes.shape({
+        //     Name: PropTypes.string.isRequired,
+        //     Description: PropTypes.string.isRequired
+        // }),
+        // Director: PropTypes.shape({
+        //     Name: PropTypes.string.isRequired,
+        //     Bio: PropTypes.string.isRequired,
+        //     Birth: PropTypes.string.isRequired
+        // }),
+        ImagePath: PropTypes.string.isRequired
+    }).isRequired
+};
