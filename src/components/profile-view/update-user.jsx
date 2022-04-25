4237a3d245870befb81f21bffafcca8de869ecfb
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-04-25 15:31:53 -0400

    Update update-user.jsx

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,41 +1,44 @@
 import React from 'react';
+import { Form, Button, Col, Row, Card, Container, FormGroup, FormControl } from 'react-bootstrap';
 
-function UpdateUser() {
+export function UpdatedUser(props) {
+    const user = props.userdata
+    const { handleSubmit, handleUpdate } = props;
+
     return (
         <Container>
-            <Row>
-                <Col>
+            <Col>
+                <Row>
                     <Card>
                         <Card.Body>
-                            <Card.Title>Profile</Card.Title>
-                            <Form className="updateform" onSubmit={(e) =>
-                                this.editUser(
+                            <Card.Title>Update Profile</Card.Title>
+                            <Form className="update-form" onSubmit={(e) => handleSubmit(e)}>
+                                {/* this.editUser(
                                     e,
                                     this.Username,
                                     this.Password,
                                     this.Email,
                                     this.Birthday
-                                )} >
-                                <FormGroup>
-                                    <Form.Label>Username</Form.Label>
+                                )} > */}
+                                <FormGroup controlId="formUsername">
+                                    <Form.Label>Username: </Form.Label>
                                     <FormControl
                                         type="text"
                                         name="Username"
                                         placeholder="New Username"
-                                        value={Username}
-                                        onChange={(e) => this.setUsername(e.target.value)}
+                                        value={user.Username}
+                                        onChange={(e) => handleUpdate(e)}
                                         required
                                     />
                                 </FormGroup>
 
-                                <FormGroup>
-                                    <Form.Label>Password</Form.Label>
+                                <FormGroup controlId="formPassword">
+                                    <Form.Label>Password:</Form.Label>
                                     <FormControl
                                         type="password"
                                         name="Password"
                                         placeholder="New password"
-                                        value=""
-                                        onChange={(e) => this.setPassword(e.target.value)}
+                                        onChange={(e) => handleUpdate(e)}
                                         required
                                     />
                                 </FormGroup>
@@ -43,40 +46,38 @@ function UpdateUser() {
                                 <FormGroup>
                                     <Form.Label>Email</Form.Label>
                                     <FormControl
-                                        type="email"
+                                        type="text"
                                         name="Email"
                                         placeholder="New Email"
-                                        value={Email}
-                                        onChange={(e) => this.setEmail(e.target.value)}
+                                        value={user.Email}
+                                        onChange={(e) => handleUpdate(e)}
                                         required
                                     />
                                 </FormGroup>
 
-                                <FormGroup>
+                                {/* <FormGroup>
                                     <Form.Label>Birthday</Form.Label>
                                     <FormControl
                                         type="date"
                                         name="Birthday"
                                         placeholder="New Birthday"
-                                        value={Birthday}
-                                        onChange={(e) => this.setBirthday(e.target.value)}
+                                        value={user.Birthday}
+                                        onChange={(e) => handleUpdate}
                                         required
                                     />
-                                </FormGroup>
+                                </FormGroup> */}
 
                                 <div>
-                                    <Button variant="success" type="submit" onClick={this.editUser}>Update Info</Button>
-                                    <Button variant="secondary" onClick={() => this.onDeleteUser()}>Delete Profile</Button>
+                                    <Button variant="success" type="submit" onClick={handleSubmit}>Update Info</Button>
+                                    {/* <Button variant="secondary" onClick={deleteProfile}>Delete Profile</Button> */}
                                 </div>
                             </Form>
                         </Card.Body>
                     </Card>
-                </Col>
-            </Row>
+                </Row>
+            </Col>
 
 
         </Container>
     )
 }
-
-export default UpdateUser
