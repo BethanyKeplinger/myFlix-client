d2e17b56b246d0c853eed34954fe0b543fad297b
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-05-05 13:13:27 -0400

    Update index.jsx
    Implementing react-redux into code

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainView from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import MainView from './components/main-view/main-view';
 
 //Import state to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+
+const store = createStore(moviesApp, devToolsEnhancer);
 
 //Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <div className='main-view'>
-                <Container>
-                    <MainView />
-                </Container>
-            </div>
+            <Provider store={store}>
+                <div className='main-view'>
+                    <Container>
+                        <MainView />
+                    </Container>
+                </div>
+            </Provider>
         );
     }
 }
